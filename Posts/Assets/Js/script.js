1e11071146f8299d34ff587cf9ac13694206d45a
Mais serhan <159805997+MaisSerhan@users.noreply.github.com>
2025-07-21 21:44:51 +0300

    Delete Posts/Assets directory

--- Posts/Assets/Js/script.js
+++
@@ -1,9 +0,0 @@
-function filterPosts() {
-  const input = document.getElementById("searchInput").value.toLowerCase();
-  const items = document.querySelectorAll("#postList a");
-
-  items.forEach(item => {
-    const text = item.textContent.toLowerCase();
-    item.style.display = text.includes(input) ? "block" : "none";
-  });
-}
